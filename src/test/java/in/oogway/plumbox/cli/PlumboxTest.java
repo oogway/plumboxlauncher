624553f53f56b8b631abe4ce5c5ac73df33ca429
Piyush Verma <github@piyushverma.net>
2018-02-22 20:34:41 +0530

    Add travis integration

--- src/test/java/in/oogway/plumbox/cli/PlumboxTest.java
+++ src/test/java/in/oogway/plumbox/cli/PlumboxTest.java
@@ -14,13 +14,6 @@ class PlumboxTest extends LocalTester {
     private String[] transformations() {
         return new String[]{
                 "in.oogway.plumbox.SampleTransformation",
-                "in.oogway.plumbox.ShowDf"
-        };
-    }
-
-    private String[] new_transformations() {
-        return new String[]{
-                "in.oogway.plumbox.ActionsTransformation",
                 "in.oogway.plumbox.cli.testing.transformation.ShowDF"
         };
     }
@@ -30,10 +23,9 @@ class PlumboxTest extends LocalTester {
         Plumbox pb = new Plumbox(new MemoryStorage());
 
         Source s = new Source(new HashMap<String, String>(){{
-            put("format", "jdbc");
-            put("driver", "com.mysql.jdbc.Driver");
-            put("url", "jdbc:mysql://127.0.0.1:3306/test_db?user=root&password=password");
-            put("dbtable", "student");
+            put("format", "json");
+            put("multiline", "true");
+            put("path", "src/test/resources/input_source_file.json");
         }});
 
         String sourceId = pb.declare(s);
@@ -57,6 +49,4 @@ class PlumboxTest extends LocalTester {
             e.printStackTrace();
         }
     }
-
 }
-
