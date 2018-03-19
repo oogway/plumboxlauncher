241875d1646a1b1b7036dc39ba503f139c202f70
Piyush Verma <github@piyushverma.net>
2018-03-19 14:51:29 +0530

    Dont need unnecessary initialization

--- src/main/java/in/oogway/plumbox/cli/Launcher.java
+++ src/main/java/in/oogway/plumbox/cli/Launcher.java
@@ -7,15 +7,15 @@ import in.oogway.plumbox.launcher.storage.RedisStorage;
 import in.oogway.plumbox.launcher.storage.LauncherStorageDriver;
 import org.apache.spark.sql.SparkSession;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.datanucleus.store.types.wrappers.backed.Map;
 
 import java.io.IOException;
 import java.util.HashMap;
 
 public class Launcher {
     private static HashMap<String, Object> makeOptions(String opt) throws IOException {
-        HashMap<String, Object> opts = new HashMap<>();
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(opt, opts.getClass());
+        return mapper.readValue(opt, HashMap.class);
     }
 
     public static void main(String args[]) throws IllegalAccessException, ClassNotFoundException, InstantiationException, IOException {
