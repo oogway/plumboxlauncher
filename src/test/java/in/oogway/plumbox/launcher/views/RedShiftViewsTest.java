b118acaf52af1f5bf06abf9a7beafe5cabc52827
Jaideep K <jdk2588@gmail.com>
2018-04-07 21:25:59 +0700

    Remove the extra lines

--- src/test/java/in/oogway/plumbox/launcher/views/RedShiftViewsTest.java
+++ src/test/java/in/oogway/plumbox/launcher/views/RedShiftViewsTest.java
@@ -1,16 +1,11 @@
 package in.oogway.plumbox.launcher.views;
 
 import in.oogway.plumbox.cli.testing.LocalTester;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder;
 import org.apache.spark.sql.types.StructType;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by jaideep Khandelwal<jaideep@oogway.in> on 06/04/18.
@@ -19,29 +14,18 @@ class RedShiftViewsTest extends LocalTester {
 
     @Before
     void setUp() {
-
-        localSession()
-                .sparkContext()
-                .hadoopConfiguration()
-                .set("fs.s3n.awsAccessKeyId","");
-
-        localSession()
-                .sparkContext()
-                .hadoopConfiguration()
-                .set("fs.s3n.awsSecretAccessKey","");
-
         System.setProperty("rs_url", "");
         System.setProperty("rs_user", "");
         System.setProperty("rs_password", "");
         System.setProperty("storage_path", "");
         System.setProperty("storage_protocol", "");
         System.setProperty("top_dir", "");
-
     }
 
     @Test
     void testViewCreation() {
         setUp();
+
         RedShiftViews redShiftViews = new RedShiftViews();
 
         StructType schema = ExpressionEncoder.javaBean(ViewBean.class).schema();
@@ -49,5 +33,6 @@ class RedShiftViewsTest extends LocalTester {
         redShiftViews.generateView(localSession(), schema.fields());
 
         redShiftViews.rePopulate();
+
     }
 }
